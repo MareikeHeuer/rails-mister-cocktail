7175c1215a70a04184be0e29ab5d2cf25f9c3ebe
Mareike Heuer <mareike.heuer@hotmail.com>
2020-11-28 06:34:56 +0100

    Generate pages home controller and add root path home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :show, :create] do 
+  resources :cocktails, only: [:index, :new, :show, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [ :destroy ]
 
 end
+
+
